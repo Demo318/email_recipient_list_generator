c2c55c6c4a8a6ddfa09d8ef2de279b6c5790eda6
Devin Mork <devin.mork@gmail.com>
2021-02-22 09:55:14 -0600

    Add opening .xlsx file.

--- generate_contact_lists.py
+++ generate_contact_lists.py
@@ -1,1 +1,10 @@
-﻿
+﻿# xl_file = pd.ExcelFile(file_name)
+
+# dfs = {sheet_name: xl_file.parse(sheet_name) 
+#           for sheet_name in xl_file.sheet_names}
+
+import pandas as pd 
+
+print('lol')
+
+xl_file = pd.ExcelFile('Master Contact List.xlsx')
