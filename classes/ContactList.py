9f8e5d3120fd3516022bc9b21cb448126c6af562
Devin Mork <devin.mork@gmail.com>
2021-02-26 16:39:34 -0600

    Adding folder

---
+++ classes/ContactList.py
@@ -0,0 +1,23 @@
+﻿import pandas
+
+class ContactList: 
+
+    def __init__(self, category_name):
+        self.category_name = category_name
+        self.list_name = category_name + "_list"
+        self.contacts_library = {
+            'First':[],'Last':[], 'Company':[],
+            'Email':[], 'BCC':[]
+            }
+
+    def add_contact(self, row):
+        for index, item in enumerate(self.contacts_library):
+            self.contacts_library[item].append(row[index])
+
+    def dataframe(self):
+        return pandas.DataFrame.from_dict(
+            self.contacts_library
+            )
+
+    def filename_string(self):
+        return ".\contact_lists\\" + self.list_name +".csv"
---
+++ generate_contact_lists.bat
@@ -0,0 +1,2 @@
+::This batch file executes the python script to generate custom contact lists.
+python generate_contact_lists.py
