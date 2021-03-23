a1ce4773e2ca181b9a785940e99f53d0ff6ba713
Devin Mork <devin.mork@gmail.com>
2021-03-23 16:46:06 -0500

    add psuedocode notes.

---
+++ send_emails.py
@@ -0,0 +1,13 @@
+
+
+# open empty email file
+
+# open recipient list
+
+# loop
+# 1. For each line
+# 2. new email object (copy of empty email?)
+# 3. grab to & bcc email
+# 3a. insert into email object
+# 4. find & replace in email body
+# 5. send email
